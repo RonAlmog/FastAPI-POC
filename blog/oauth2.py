15e72a4ec3e1c3abef9aac7b9a56d2a12ec10dfb
ron-afs <94569277+ron-afs@users.noreply.github.com>
2022-03-06 18:48:35 -0500

    all routes protected

--- blog/oauth2.py
+++ blog/oauth2.py
@@ -6,14 +6,14 @@ from . import token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token_str: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-    return token.verify_token(token, credentials_exception)
+    return token.verify_token(token_str, credentials_exception)
 
     # user = get_user(fake_users_db, username=token_data.username)
     # if user is None:
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -15,25 +15,25 @@ get_db = database.get_db
 
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog], tags=['blogs'])
-def getall(db: Session = Depends(get_db), get_current_user: schemas.User = Depends(oauth2.get_current_user)):
+def getall(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: int, db: Session = Depends(get_db)):
+def delete(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.delete(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, requst: schemas.Blog, db: Session = Depends(get_db)):
+def update(id: int, requst: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request, db)
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def getall(id: int, db: Session = Depends(get_db)):
+def getall(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id, db)
